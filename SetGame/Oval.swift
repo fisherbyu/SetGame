40dc2990819654fc7c49f6843c60cc90436f3f01
fisherbyu <fisherandrew777@gmail.com>
2023-10-28 15:43:03 -0600

    Add Oval Shape

---
+++ SetGame/Oval.swift
@@ -0,0 +1,18 @@
+//
+//  Oval.swift
+//  SetGame
+//
+//  Created by Andrew Fisher on 10/28/23.
+//
+
+import SwiftUI
+
+struct Oval: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Oval()
+}
